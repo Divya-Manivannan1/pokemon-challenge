581c1c3e24172695f8dd06da27c066b03a46641d
Divya-Manivannan1 <reethudya@gmail.com>
2024-03-13 12:42:20 +0000

    DisplayByName and No of results work seperately

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import "./styles/style.scss";
 import { pokemonArray } from "./data/pokemon";
 import { Pokemon } from "./data/types";
 
-const cardContainer = document.querySelector(".card-container");
+const cardContainer = document.querySelector<HTMLDivElement>(".card-container");
+const searchByName = document.querySelector<HTMLInputElement>("#searchByName");
+const noOfResults = document.querySelector<HTMLInputElement>("#noOfResults");
 
-if (!cardContainer) {
+if (!cardContainer || !searchByName || !noOfResults) {
   throw new Error("Problem with the selector");
 }
 
@@ -22,4 +24,34 @@ const displayPokemonCard = (pokemon: Pokemon): void => {
       </article>`;
 };
 
+noOfResults.placeholder = `${pokemonArray.length}`;
+
 pokemonArray.forEach(displayPokemonCard);
+
+const displayFilteredCardsByName = (event: Event): void => {
+  noOfResults.value = "";
+  const textBox = event.currentTarget as HTMLInputElement;
+  const searchTerm: string = textBox.value.toLowerCase();
+  const filteredPokemons: Pokemon[] = pokemonArray.filter((pokemon): boolean =>
+    pokemon.name.includes(searchTerm)
+  );
+  cardContainer.innerHTML = "";
+  filteredPokemons.forEach(displayPokemonCard);
+};
+
+const handleNoOfResults = (event: Event): void => {
+  searchByName.value = "";
+  pokemonArray.forEach(displayPokemonCard);
+  const noField = event.currentTarget as HTMLInputElement;
+  const displayNum: number = +noField.value;
+  while (cardContainer.children.length > displayNum) {
+    const cardToBeRemoved = cardContainer.lastChild as HTMLElement;
+    if (!cardToBeRemoved) {
+      throw new Error("Problem with card container child selector");
+    }
+    cardContainer.removeChild(cardToBeRemoved);
+  }
+};
+
+searchByName.addEventListener("input", displayFilteredCardsByName);
+noOfResults.addEventListener("input", handleNoOfResults);
